2c44762ad7559343699237bed0dc26693476fc11
Suzanne Rosenzweig <srosenzweig09@gmail.com>
2021-03-25 09:17:31 -0400

    Mostly matching numbers and plots.

--- Analysis_2021/studies/analyze_performance.py
+++ Analysis_2021/studies/analyze_performance.py
@@ -19,6 +19,7 @@ config.read(config_file)
 
 HtoLL    = config['HtoLL']['filename']
 treename = config['HtoLL']['treename']
+print(HtoLL)
 
 tree = get_uproot_Table(HtoLL, treename)
 
@@ -26,10 +27,11 @@ nevents = len(tree['genPart_pt'])
 muon = (abs(tree['genPart_ID']) == 13) & (tree['genPart_parentID'] == 6000113)
 nmuons = len(tree['genPart_pt'][muon].flatten())
 
+print("gen muons",nmuons)
+
 gen_emtf_matching  = np.load('matching/gen_gmt/gen_gmt_matching_masks_0pt6.npz', allow_pickle=True)
 gen_index = gen_emtf_matching['gen_indices']
 emtf_index = gen_emtf_matching['emtf_indices']
-gmt_index = gen_emtf_matching['gmt_indices']
 
 #############################################################
 ## Matched gen kinematics
@@ -42,7 +44,9 @@ matched_gen_vy  = tree['genPart_vy'] [muon].flatten()[gen_index]
 matched_gen_vz  = tree['genPart_vz'] [muon].flatten()[gen_index]
 matched_gen_q   = tree['genPart_q']  [muon].flatten()[gen_index]
 
-matched_gen_eta, matched_gen_phi = calcStar(matched_gen_eta, matched_gen_phi, matched_gen_vx, matched_gen_vy, matched_gen_vz, is2darray=False)
+print("matched gen/emtf",len(matched_gen_pt))
+
+matched_gen_eta, matched_gen_phi = calcStar(matched_gen_eta, matched_gen_phi, matched_gen_vx, matched_gen_vy, matched_gen_vz)
 
 matched_gen_d0 = calc_d0(matched_gen_pt, matched_gen_phi, matched_gen_vx, matched_gen_vy, matched_gen_q, B=3.811)
 
@@ -50,7 +54,6 @@ matched_gen_d0 = calc_d0(matched_gen_pt, matched_gen_phi, matched_gen_vx, matche
 # Raw gen kinematics
 
 gen_pt  = tree['genPart_pt'] [muon].flatten()
-# print(len(gen_pt))
 gen_phi = tree['genPart_phi'][muon].flatten()
 gen_eta = tree['genPart_eta'][muon].flatten()
 gen_vx  = tree['genPart_vx'] [muon].flatten()
@@ -58,72 +61,41 @@ gen_vy  = tree['genPart_vy'] [muon].flatten()
 gen_vz  = tree['genPart_vz'] [muon].flatten()
 gen_q   = tree['genPart_q']  [muon].flatten()
 
-gen_eta, gen_phi = calcStar(gen_eta, gen_phi, gen_vx, gen_vy, gen_vz, is2darray=False)
+gen_eta, gen_phi = calcStar(gen_eta, gen_phi, gen_vx, gen_vy, gen_vz)
 
 gen_d0 = calc_d0(gen_pt, gen_phi, gen_vx, gen_vy, gen_q, B=3.811)
 
 #############################################################
 # pT from EMTF
 
-BDT_pt, L1_eta, L1_phi = convert_emtf(tree['emtfTrack_pt'],  tree['emtfTrack_eta'], tree['emtfTrack_phi'], flatten=True)
-
-BDT_pt = BDT_pt[emtf_index]
+BDT_pt = tree['emtfTrack_pt'].flatten()[emtf_index]
 NN_pt = tree['emtfTrack_pt_dxy'].flatten()[emtf_index]
-NN_eta = tree['gmtMuon_eta'].flatten()[gmt_index]
-
-BDT_mask = BDT_pt > 20 # GeV
-NN_mask  = NN_pt > 20 # GeV
-
-#############################################################
-# 1D Distributions
-
-ptbins = np.linspace(0, 80, 40)
-
-fig, axs = plt.subplots(nrows=1, ncols=2)
-
-hist(axs[0], NN_pt, bins=ptbins)
-hist(axs[1], matched_gen_pt, bins=ptbins)
-
-axs[0].set_xlabel("NN pT [GeV]")
-axs[1].set_xlabel("gen pT [GeV]")
-
-fig.savefig('distributions/matched_pt')
-
-fig, ax = plt.subplots()
-bins = np.linspace(-3, 3, 40)
-# hist(ax, BDT_pt, bins=bins)
-hist(ax, NN_eta, bins=bins)
-fig.savefig('distributions/matched_nn_eta')
-
-fig, ax = plt.subplots()
-bins = np.linspace(0, 80, 40)
-# hist(ax, BDT_pt, bins=bins)
-hist2d(ax, NN_pt, matched_gen_pt, bins, bins)
-fig.savefig('distributions/matched_pt_scatter')
-
-fig, ax = plt.subplots()
-bins = np.linspace(-3, 3, 40)
-# hist(ax, BDT_pt, bins=bins)
-hist2d(ax, NN_eta, matched_gen_eta, bins, bins)
-fig.savefig('distributions/matched_eta_scatter')
 
 #############################################################
 # Efficiency plots
 
-matched_gen_eta_mask = (abs(matched_gen_eta) > 1.2) & (abs(matched_gen_eta) < 2.4)
+matched_gen_eta_mask = (abs(matched_gen_eta) > 1.24) & (abs(matched_gen_eta) < 2.5)
 matched_gen_z0_d0_mask =  (abs(matched_gen_d0) < 100) & (abs(matched_gen_vz) < 100) # cm, cm
 matched_gen_pt_mask = matched_gen_pt > 15 # GeV
 
 gen_eta_mask = (abs(gen_eta) > 1.24) & (abs(gen_eta) < 2.5)
-gen_z0_d0_mask =  (abs(gen_d0) < 100) & (abs(gen_vz) < 100) # cm, cm
+gen_z0_d0_mask = (abs(gen_d0) < 100) & (abs(gen_vz) < 100) # cm, cm
 gen_pt_mask = gen_pt > 15 # GeV
+
+print("gen w/ cuts",np.sum((gen_eta_mask & gen_z0_d0_mask)*1))
+
+BDT_mask = BDT_pt > 20 # GeV
+NN_mask  = NN_pt > 20 # GeV
 
 # fig, ax = plt.subplots()
 bdt_mask = BDT_mask &  matched_gen_eta_mask & matched_gen_z0_d0_mask
-nn_mask = (NN_mask) &  matched_gen_eta_mask & matched_gen_z0_d0_mask
+nn_mask = NN_mask &  matched_gen_eta_mask & matched_gen_z0_d0_mask
 comb_mask = (BDT_mask | NN_mask) &  matched_gen_eta_mask & matched_gen_z0_d0_mask
 denom_mask = gen_eta_mask & gen_z0_d0_mask
-print(np.sum((comb_mask)*1))
+print("match emtf/gen w/ cuts",(np.sum((matched_gen_eta_mask & matched_gen_z0_d0_mask)*1)))
+print("nn cuts",np.sum(nn_mask*1))
+print("bdt cuts",np.sum(bdt_mask*1))
+print("comb cuts",np.sum(comb_mask*1))
 
 ptbins = np.linspace(0, 60, 30)
 n_comb, n_edges = np.histogram(matched_gen_pt[comb_mask].flatten(), bins=ptbins)
@@ -136,8 +108,8 @@ eff_bdt = np.where(n_den == 0, 0, n_bdt/n_den)
 eff_nn = np.where(n_den == 0, 0, n_nn/n_den)
 
 x = (n_edges[1:] + n_edges[:-1]) / 2
-fig, ax = plt.subplots()
-ax.set_ylim(0,1.2)
+fig, ax = plt.subplots(figsize=(10,10))
+ax.set_ylim(0,1.05)
 ax.set_xlabel(r'gen muon $p_T$ [GeV]')
 ax.set_ylabel('L1T Efficiency')
 
@@ -146,21 +118,53 @@ err_bdt = np.sqrt(eff_bdt*(1-eff_bdt) / n_den)
 err_nn = np.sqrt(eff_nn*(1-eff_nn) / n_den)
 
 for i,w in enumerate(eff_comb):
-    ax.plot([ptbins[i], ptbins[i+1]], [w,w], color='C0', label='Combined')
-    ax.plot([x[i], x[i]], [w-err_comb[i], w+err_comb[i]], color='C0')
+    ax.plot([ptbins[i], ptbins[i+1]], [w,w], color='blue', label='Combined')
+    ax.plot([x[i], x[i]], [w-err_comb[i], w+err_comb[i]], color='blue')
 for i,w in enumerate(eff_bdt):
-    ax.plot([ptbins[i], ptbins[i+1]], [w,w], color='C1', label='BDT')
-    ax.plot([x[i], x[i]], [w-err_comb[i], w+err_comb[i]], color='C1')
+    ax.plot([ptbins[i], ptbins[i+1]], [w,w], color='black', label='BDT')
+    ax.plot([x[i], x[i]], [w-err_comb[i], w+err_comb[i]], color='black')
 for i,w in enumerate(eff_nn):
-    ax.plot([ptbins[i], ptbins[i+1]], [w,w], color='C2',  label='NN')
-    ax.plot([x[i], x[i]], [w-err_comb[i], w+err_comb[i]], color='C2')
+    ax.plot([ptbins[i], ptbins[i+1]], [w,w], color='red',  label='NN')
+    ax.plot([x[i], x[i]], [w-err_comb[i], w+err_comb[i]], color='red')
 
 from matplotlib.lines import Line2D
-custom_lines = [Line2D([0], [0], color='C0', lw=2),
-                Line2D([0], [0], color='C1', lw=2),
-                Line2D([0], [0], color='C2', lw=2)]
+custom_lines = [Line2D([0], [0], color='blue', lw=2),
+                Line2D([0], [0], color='black', lw=2),
+                Line2D([0], [0], color='red', lw=2)]
 
 ax.legend(custom_lines, ['Combined', 'BDT', 'NN'])
 
-fig.savefig('efficiencies/efficiency_pt', bbox_inches=None)
+fig.savefig('efficiencies/efficiency_pt.pdf', bbox_inches=None)
 
+#############################################################
+# 1D Distributions
+
+# ptbins = np.linspace(0, 80, 40)
+
+# fig, axs = plt.subplots(nrows=1, ncols=2)
+
+# hist(axs[0], NN_pt, bins=ptbins)
+# hist(axs[1], matched_gen_pt, bins=ptbins)
+
+# axs[0].set_xlabel("NN pT [GeV]")
+# axs[1].set_xlabel("gen pT [GeV]")
+
+# fig.savefig('distributions/matched_pt')
+
+# fig, ax = plt.subplots()
+# bins = np.linspace(-3, 3, 40)
+# # hist(ax, BDT_pt, bins=bins)
+# hist(ax, NN_eta, bins=bins)
+# fig.savefig('distributions/matched_nn_eta')
+
+# fig, ax = plt.subplots()
+# bins = np.linspace(0, 80, 40)
+# # hist(ax, BDT_pt, bins=bins)
+# hist2d(ax, NN_pt, matched_gen_pt, bins, bins)
+# fig.savefig('distributions/matched_pt_scatter')
+
+# fig, ax = plt.subplots()
+# bins = np.linspace(-3, 3, 40)
+# # hist(ax, BDT_pt, bins=bins)
+# hist2d(ax, NN_eta, matched_gen_eta, bins, bins)
+# fig.savefig('distributions/matched_eta_scatter')
---
+++ Analysis_2021/studies/makeMatchedNtuples.C
@@ -0,0 +1,522 @@
+/* L1T analysis package 
+ * module: main module of Orthogonal dataset method 
+ * Author: George Karathanasis, georgios.karathanasis@cern.ch
+ *
+ *
+ */
+
+#include <iostream>
+#include "TH1F.h"
+#include "TFile.h"
+#include "TDirectory.h"
+#include "TTree.h"
+#include "TChain.h"
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include "TString.h"
+#include "TTreeReader.h"
+#include "TTreeReaderValue.h"
+#include "TTreeReaderArray.h"
+#include "TCanvas.h"
+#include "TH1F.h"
+#include "TH2F.h"
+// #include "analysis.h"
+// #include "CutReader.h"
+
+float DPhi(double phi1,double phi2){
+  float temp=phi1-phi2;
+  if (temp>3.14) temp=temp-6.28;
+  if (temp<-3.14) temp=temp+6.28;
+  return temp;
+}
+
+int makeMatchedNtuples(){
+
+  TString file = "/eos/cms/store/user/eyigitba/emtf/L1Ntuples/Run3/crabOut/HTo2LongLivedTo4mu_combined/EMTFNtuple_HTo2LLTo4Mu_combined_cmssw_11_0_2_fwImplementation_NNv5.root";
+
+  // load trees
+  TString tree = "EMTFNtuple/tree";
+
+  TFile *fout =new TFile("matchedNtuple_HTo2LLTo4Mu_combined_cmssw_11_0_2_fwImplementation_NNv5.root","RECREATE");
+  TTree * t1 =new TTree("tree","tree");
+
+
+  TChain * cc=new TChain(tree);
+
+
+  cc->Add(file);
+
+  TTreeReader reader(cc);
+
+  TTreeReaderValue<int32_t> emtfTrackSize(reader,"emtfTrack_size");
+  TTreeReaderArray<float  > emtfTrackPt(reader,"emtfTrack_pt");
+  TTreeReaderArray<float  > emtfTrackPtDxy(reader,"emtfTrack_pt_dxy");
+  TTreeReaderArray<float  > emtfTrackDxy(reader,"emtfTrack_dxy");
+  TTreeReaderArray<float  > emtfTrackPhi(reader,"emtfTrack_phi");
+  TTreeReaderArray<float  > emtfTrackTheta(reader,"emtfTrack_theta");
+  TTreeReaderArray<float  > emtfTrackEta(reader,"emtfTrack_eta");
+  TTreeReaderArray<int    > emtfTrackGMTPhi(reader,"emtfTrack_GMT_phi");
+  TTreeReaderArray<int    > emtfTrackGMTEta(reader,"emtfTrack_GMT_eta");
+  TTreeReaderArray<short  > emtfTrackMode(reader,"emtfTrack_mode");
+  TTreeReaderArray<short  > emtfTrackEndcap(reader,"emtfTrack_endcap");
+  TTreeReaderArray<short  > emtfTrackSector(reader,"emtfTrack_sector");
+  TTreeReaderArray<short  > emtfTrackBX(reader,"emtfTrack_bx");
+
+
+  TTreeReaderValue<int32_t> gmtMuonSize(reader,"gmtMuon_size");
+  TTreeReaderArray<float  > gmtMuonPt(reader,"gmtMuon_pt");
+  TTreeReaderArray<float  > gmtMuonPtDxy(reader,"gmtMuon_pt_dxy");
+  TTreeReaderArray<float  > gmtMuonPtDxyNN(reader,"gmtMuon_pt_dxyNN");
+  TTreeReaderArray<short  > gmtMuonDxy(reader,"gmtMuon_dxy");
+  TTreeReaderArray<float  > gmtMuonDxyNN(reader,"gmtMuon_dxyNN");
+  TTreeReaderArray<float  > gmtMuonPhi(reader,"gmtMuon_phi");
+  TTreeReaderArray<float  > gmtMuonEta(reader,"gmtMuon_eta");
+  TTreeReaderArray<short  > gmtMuonCharge(reader,"gmtMuon_q");
+  TTreeReaderArray<short  > gmtMuonQual(reader,"gmtMuon_qual");
+
+
+  TTreeReaderArray<short  > genPartCharge(reader,"genPart_q");
+  TTreeReaderArray<float  > genPartPt(reader,"genPart_pt");
+  TTreeReaderArray<float  > genPartDxy(reader,"genPart_dxy");
+  TTreeReaderArray<float  > genPartEta(reader,"genPart_eta");
+  TTreeReaderArray<float  > genPartPhi(reader,"genPart_phi");
+  TTreeReaderArray<short  > genPartID(reader,"genPart_ID");
+  TTreeReaderArray<int32_t> genPartParentID(reader,"genPart_parentID");
+  TTreeReaderArray<float  > genPartVx(reader,"genPart_vx");
+  TTreeReaderArray<float  > genPartVy(reader,"genPart_vy");
+  TTreeReaderArray<float  > genPartVz(reader,"genPart_vz");
+
+  std::vector<float> l1_pt, l1_ptDxy, l1_eta, l1_phi, l1_qual, l1_dxyNN,
+                     gen_pt, gen_eta, gen_phi, gen_etaStar, gen_phiStar, gen_Lxy, gen_dR, gen_vx, gen_vy, gen_vz, gen_d0,
+                     gendimu_pt, gendimu_eta, gendimu_phi, gendimu_Lxy, gendimu_vz;
+  std::vector<int>   gen_parent, gen_idx, gen_charge, gen_matchedL1Mu, l1_dxy, l1_charge, l1_emtfMode, gendimu_daughter1, gendimu_daughter2, gmtTOgen_idx, emtfTOgmt_idx;
+
+  t1->Branch("l1_pt",& l1_pt);
+  t1->Branch("l1_ptDxy",& l1_ptDxy);
+  t1->Branch("l1_eta",& l1_eta);
+  t1->Branch("l1_phi",& l1_phi);
+  t1->Branch("l1_qual",& l1_qual);
+  t1->Branch("l1_dxyNN",& l1_dxyNN);
+  t1->Branch("l1_dxy",& l1_dxy);
+  t1->Branch("l1_charge",& l1_charge);
+  t1->Branch("l1_emtfMode",& l1_emtfMode);
+
+  t1->Branch("gen_pt",&gen_pt);
+  t1->Branch("gen_eta",&gen_eta);
+  t1->Branch("gen_etaStar",&gen_etaStar);
+  t1->Branch("gen_phi",&gen_phi);
+  t1->Branch("gen_phiStar",&gen_phiStar);
+  t1->Branch("gen_Lxy",&gen_Lxy);
+  t1->Branch("gen_dR",&gen_dR);
+  t1->Branch("gen_vx",&gen_vx);
+  t1->Branch("gen_vy",&gen_vy);
+  t1->Branch("gen_vz",&gen_vz);
+  t1->Branch("gen_d0",&gen_d0);
+  t1->Branch("gen_parent",&gen_parent);
+  t1->Branch("gen_idx",&gen_idx);
+  t1->Branch("gen_charge",&gen_charge);
+  t1->Branch("gen_matchedL1Mu",&gen_matchedL1Mu);
+  t1->Branch("gmtTOgen_idx",&gmtTOgen_idx);
+  t1->Branch("emtfTOgmt_idx",&emtfTOgmt_idx);
+
+  t1->Branch("gendimu_pt",&gendimu_pt);
+  t1->Branch("gendimu_eta",&gendimu_eta);
+  t1->Branch("gendimu_phi",&gendimu_phi);
+  t1->Branch("gendimu_Lxy",&gendimu_Lxy);
+  t1->Branch("gendimu_vz",&gendimu_vz);
+  t1->Branch("gendimu_daughter1",&gendimu_daughter1);
+  t1->Branch("gendimu_daughter2",&gendimu_daughter2);
+
+
+
+
+  int eventCount = 0;
+  int matchedGMT = 0;
+  int matchedEMTF = 0;
+  int extras = 0;
+
+  float z_ME2 = 808.0; // ME2 z coordinate in [cm]
+
+  float r = 0.;
+  float rStar = 0.;
+  
+  float etaStar_gen = 0.;
+
+  float phiStar_gen = 0.;
+
+  float dR, l1mu_idx, dR_EMTF, idx_EMTF;
+  std::vector<float> dR_gmt;
+  std::vector<int> gmt_idx;
+
+  while(reader.Next()){
+    // if (eventCount > 0) { break; }
+    if (eventCount % 10000 == 0) {
+      std::cout << eventCount << " events read!" << std::endl;
+    //   std::cout << matchedGMT << " vs " << matchedEMTF << std::endl;
+    } 
+    // if (eventCount < 50) {
+    //   std::cout << eventCount << " events read!" << std::endl;
+    //   std::cout << matchedGMT << " vs " << matchedEMTF << std::endl;
+    // } 
+    eventCount++;
+
+    // clear stuff
+    l1_pt.clear();
+    l1_ptDxy.clear();
+    l1_eta.clear();
+    l1_phi.clear();
+    l1_qual.clear();
+    l1_charge.clear();
+    l1_emtfMode.clear();
+    l1_dxy.clear();
+    l1_dxyNN.clear();
+
+    gen_pt.clear();
+    gen_eta.clear();
+    gen_etaStar.clear();
+    gen_phi.clear();
+    gen_phiStar.clear();
+    gen_Lxy.clear();
+    gen_dR.clear();
+    gen_vx.clear();
+    gen_vy.clear();
+    gen_vz.clear();
+    gen_parent.clear();
+    gen_idx.clear();
+    gen_matchedL1Mu.clear();
+    gen_d0.clear();
+    gen_charge.clear();
+
+    gendimu_pt.clear();
+    gendimu_Lxy.clear();
+    gendimu_eta.clear();
+    gendimu_phi.clear();
+    gendimu_vz.clear();
+    gendimu_daughter1.clear();
+    gendimu_daughter2.clear();
+
+    dR_gmt.clear();
+    gmt_idx.clear();
+    gmtTOgen_idx.clear();
+    emtfTOgmt_idx.clear();
+
+    // Initialize a vector to keep track of gen indices matched to gmt (initalized to -1)
+    for (int j=0; j<*gmtMuonSize; j++){
+      gmtTOgen_idx.push_back(-1);
+    } 
+    for (int j=0; j<*emtfTrackSize; j++){
+      emtfTOgmt_idx.push_back(-1);
+    } 
+
+    // loop over GEN muons
+    int ngen = 0;
+    for (int i=0; i<genPartPt.GetSize(); i++){
+      if(abs(genPartID[i]) != 13 ) continue;
+      if(abs(genPartParentID[i]) != 6000113 ) continue;
+      ngen++;
+
+      // convert displaced eta and phi to prompt-like and calculate dR between gen and l1 muon
+      r = (z_ME2 - abs(genPartVz[i]))/abs(TMath::SinH(genPartEta[i]));
+      float xStar = genPartVx[i] + r * TMath::Cos(genPartPhi[i]);
+      float yStar = genPartVy[i] + r * TMath::Sin(genPartPhi[i]);
+      rStar = TMath::Sqrt(xStar * xStar + yStar * yStar);
+      
+      etaStar_gen = TMath::ASinH(z_ME2/rStar) * (genPartEta[i]/abs(genPartEta[i]));
+
+      // if (abs(etaStar_gen) < 1.2 || abs(etaStar_gen) > 2.5 ) continue;
+
+      if (xStar >= 0) phiStar_gen = TMath::ATan(yStar/xStar); 
+      else if (yStar >= 0 && xStar < 0) phiStar_gen = TMath::Pi() + TMath::ATan(yStar/xStar); 
+      else if (yStar <= 0 && xStar < 0) phiStar_gen = TMath::ATan(yStar/xStar) - TMath::Pi(); 
+
+      float d0 = -999.0;
+      float invPt = genPartCharge[i]/genPartPt[i];
+      invPt = (std::abs(invPt) < 1./10000) ? (invPt < 0 ? -1./10000 : +1./10000) : invPt;
+      double R = -1.0 / (0.003 * 3.811 * invPt);
+      float xc = genPartVx[i] - (R * std::sin(genPartPhi[i]));
+      float yc = genPartVy[i] + (R * std::cos(genPartPhi[i]));
+      d0 = R - ((R < 0 ? -1. : +1.) * TMath::Sqrt(xc*xc + yc*yc));
+
+      float Lxy = TMath::Sqrt(genPartVx[i]*genPartVx[i] + genPartVy[i]*genPartVy[i]);
+
+      gen_pt.push_back(genPartPt[i]);
+      gen_eta.push_back(genPartEta[i]);
+      gen_phi.push_back(genPartPhi[i]);
+      gen_Lxy.push_back(Lxy);
+      gen_etaStar.push_back(etaStar_gen);
+      gen_phiStar.push_back(phiStar_gen);
+      gen_vx.push_back(genPartVx[i]);
+      gen_vy.push_back(genPartVy[i]);
+      gen_vz.push_back(genPartVz[i]);
+      gen_parent.push_back(genPartParentID[i]);
+      gen_idx.push_back(i);
+      gen_d0.push_back(d0);
+      gen_charge.push_back(genPartCharge[i]);
+
+      // std::cout << "etastar " << etaStar_gen << std::endl;
+      // std::cout << "phistar " << phiStar_gen << std::endl;
+
+      // GEN muon GMT muon matching and GMT muon EMTF track matching
+      dR = 5.0;
+      l1mu_idx = -99;
+      // Loop over gmt muons and find matching gen muon
+      for (unsigned int j=0; j<*gmtMuonSize; j++){
+        // if (abs(gmtMuonEta[i]) < 1.2) continue;
+
+        float dR_new = TMath::Sqrt((gmtMuonEta[j]-etaStar_gen)*(gmtMuonEta[j]-etaStar_gen)+DPhi(gmtMuonPhi[j],phiStar_gen)*DPhi(gmtMuonPhi[j],phiStar_gen));
+        // std::cout << "dR GEN: " << dR_new << std::endl;
+        if(dR_new > dR){
+          continue;
+        }
+        else{
+          l1mu_idx = j;
+          dR = dR_new;
+        }
+      }
+
+      gen_dR.push_back(dR);
+
+      bool pass_flag = true;
+      // Only save as matched if dR < 0.6 and it has not been matched to a better candidate
+      if (dR < 0.6) {
+        for (unsigned int k=0; k<i; k++) {
+          // Check all gen muon matched gmt indices to see if a gen muon has already been matched to our candidate gmt muon
+          if (gen_matchedL1Mu[k] == l1mu_idx && l1mu_idx != -99) {
+            // Earlier match found!
+            // Check if earlier match had lower dR
+            if (gen_dR[k] < dR) {
+              // Earlier gen match stays
+              // This gmt is not matched 
+              gen_matchedL1Mu.push_back(-1);
+              pass_flag = false;
+
+            }
+            else {
+              gen_matchedL1Mu[k] = -1;
+              matchedGMT--;
+            }
+          }
+          for (unsigned int k=0; k<*gmtMuonSize; k++) {
+          if (gmtTOgen_idx[k] == i) {
+            // Check if earlier match was lower
+            if (gen_dR[i] < dR) {
+              // Earlier gen match stays
+              // This gmt is not matched 
+              pass_flag = false;
+            }
+            else {
+              gmtTOgen_idx[k] = -1;
+              matchedGMT--;
+            }
+          }
+          }
+        }
+        if (pass_flag) {
+          gen_matchedL1Mu.push_back(l1mu_idx);
+          gmtTOgen_idx[l1mu_idx] = i;
+          matchedGMT++;
+        }
+      } 
+      else {
+        gen_matchedL1Mu.push_back(-1);
+      }
+    } 
+
+    // create GEN dimuon parent
+    for (int i=0; i<genPartPt.GetSize(); i++){
+
+      if(genPartID[i] != 13 ) continue;
+      if(abs(genPartParentID[i]) != 6000113 ) continue;
+
+      float Lxy_i = TMath::Sqrt(genPartVx[i]*genPartVx[i] + genPartVy[i]*genPartVy[i]);
+
+
+      for (int j=0; j<genPartPt.GetSize(); j++){
+        if (i == j) continue;
+        if(genPartID[j] != -13 ) continue;
+        if(abs(genPartParentID[j]) != 6000113 ) continue;
+        
+        float Lxy_j = TMath::Sqrt(genPartVx[j]*genPartVx[j] + genPartVy[j]*genPartVy[j]);
+
+        if (Lxy_i != Lxy_j) continue;
+        if (genPartVz[i] != genPartVz[j]) continue;
+
+        TLorentzVector mu1, mu2, parent;
+
+        mu1.SetPtEtaPhiM(genPartPt[i], genPartEta[i], genPartPhi[i], 0.1057);
+        mu2.SetPtEtaPhiM(genPartPt[j], genPartEta[j], genPartPhi[j], 0.1057);
+
+        parent = mu1 + mu2;
+
+        gendimu_pt.push_back(parent.Pt());
+        gendimu_eta.push_back(parent.Eta());
+        gendimu_phi.push_back(parent.Phi());
+        gendimu_Lxy.push_back(Lxy_i);
+        gendimu_vz.push_back(genPartVz[i]);
+        gendimu_daughter1.push_back(i);
+        gendimu_daughter2.push_back(j);
+        
+
+      }
+
+
+    }
+    
+    // Find EMTF tracks matched to GMT muon and fill L1 muon info 
+    for (int i=0; i<*gmtMuonSize; i++){
+      if (gmtTOgen_idx[i] == -1){
+        dR_gmt.push_back(dR_EMTF);
+        gmt_idx.push_back(-1);
+        l1_pt.push_back(-9999);
+        l1_ptDxy.push_back(-9999);
+        l1_eta.push_back(-9999);
+        l1_phi.push_back(-9999);
+        l1_qual.push_back(-9999);
+        l1_dxy.push_back(-9999);
+        l1_dxyNN.push_back(-9999);
+        l1_charge.push_back(int(-9999));
+        l1_emtfMode.push_back(-9999);
+        continue;
+      } 
+      // if(abs(l1mueta[i]) < 1.24 or abs(l1mueta[i]) > 2.5) continue;
+      
+      int gen_id = -1;
+      float dR = -999.;
+
+      dR_EMTF = 5.0;
+      idx_EMTF = -99;
+      // GMT muon EMTF track matching
+      for (int j=0; j<*emtfTrackSize; j++){
+        if(emtfTrackBX[j] != 0) continue;
+
+        float GMTEta = emtfTrackGMTEta[j] * 0.010875;
+
+        int globPhi = (emtfTrackSector[j] - 1) * 96 + emtfTrackGMTPhi[j];
+        globPhi = (globPhi + 600) % 576;
+        float GMTPhi = globPhi * 0.010908;
+
+        float dR_new_EMTF = TMath::Sqrt((gmtMuonEta[i]-GMTEta)*(gmtMuonEta[i]-GMTEta)+DPhi(gmtMuonPhi[i],GMTPhi)*DPhi(gmtMuonPhi[i],GMTPhi));
+
+        if(dR_new_EMTF > dR_EMTF){
+          continue;
+        }
+        else{
+          idx_EMTF = j;
+          dR_EMTF = dR_new_EMTF;
+        }
+
+      }
+
+      if (idx_EMTF < 0){
+        dR_gmt.push_back(dR_EMTF);
+        gmt_idx.push_back(-1);
+        l1_pt.push_back(-9999);
+        l1_ptDxy.push_back(-9999);
+        l1_eta.push_back(-9999);
+        l1_phi.push_back(-9999);
+        l1_qual.push_back(-9999);
+        l1_dxy.push_back(-9999);
+        l1_dxyNN.push_back(-9999);
+        l1_charge.push_back(int(-9999));
+        l1_emtfMode.push_back(-9999);
+      } 
+
+      else{
+        int GMT_dxy = -1;
+
+        if (abs(emtfTrackDxy[idx_EMTF]) < 25){
+          GMT_dxy = 0;
+        }
+        else if (abs(emtfTrackDxy[idx_EMTF]) < 50){
+          GMT_dxy = 1;
+        }
+        else if (abs(emtfTrackDxy[idx_EMTF]) < 75){
+          GMT_dxy = 2;
+        }
+        else {
+          GMT_dxy = 3;
+        }
+
+        bool emtf_flag = true;
+        for (unsigned int k=0; k<i; k++) {
+          // Check all gen muon matched gmt indices to see if a gen muon has already been matched to our candidate gmt muon
+          if (gmt_idx[k] == idx_EMTF) {
+            // Earlier match found!
+            // Check if earlier match had lower dR
+            if (dR_gmt[k] < dR_EMTF) {
+              // Earlier gen match stays
+              // This gmt is not matched 
+              gmt_idx.push_back(-1);
+              emtf_flag = false;
+
+            }
+            else {
+              gmt_idx[k] = -1;
+              matchedEMTF--;
+            }
+          }
+          for (unsigned int k=0; k<*emtfTrackSize; k++) {
+          if (emtfTOgmt_idx[k] == i) {
+            // Check if earlier match was lower
+            if (dR_gmt[i] < dR_EMTF) {
+              // Earlier gen match stays
+              // This gmt is not matched 
+              emtf_flag = false;
+            }
+            else {
+              emtfTOgmt_idx[k] = -1;
+              matchedEMTF--;
+            }
+          }
+          }
+        }
+
+        if (emtf_flag) {
+          gmt_idx.push_back(idx_EMTF);
+          dR_gmt.push_back(dR_EMTF);
+          l1_pt.push_back(emtfTrackPt[idx_EMTF]);
+          l1_ptDxy.push_back(emtfTrackPtDxy[idx_EMTF]);
+          l1_eta.push_back(gmtMuonEta[i]);
+          l1_phi.push_back(gmtMuonPhi[i]);
+          l1_qual.push_back(gmtMuonQual[i]);
+          l1_dxy.push_back(int(GMT_dxy));
+          l1_dxyNN.push_back(emtfTrackDxy[idx_EMTF]);
+          l1_charge.push_back(int(gmtMuonCharge[i]));
+          l1_emtfMode.push_back(int(emtfTrackMode[idx_EMTF]));
+          matchedEMTF++;
+        }
+        else {
+          dR_gmt.push_back(dR_EMTF);
+          gmt_idx.push_back(-1);
+          l1_pt.push_back(-9999);
+          l1_ptDxy.push_back(-9999);
+          l1_eta.push_back(-9999);
+          l1_phi.push_back(-9999);
+          l1_qual.push_back(-9999);
+          l1_dxy.push_back(-9999);
+          l1_dxyNN.push_back(-9999);
+          l1_charge.push_back(int(-9999));
+          l1_emtfMode.push_back(-9999);
+        }
+
+        
+      }
+    }
+
+        
+        
+
+    
+    t1->Fill();
+
+  } // end event loop
+  
+  std::cout << matchedGMT << " vs " << matchedEMTF << std::endl;
+
+  t1->Write();
+
+  return 0;
+} // end function
