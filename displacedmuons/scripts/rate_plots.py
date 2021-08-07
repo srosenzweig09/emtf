e8978c9136752ccf8c5dafd873b616ba63f6e60c
Suzanne Rosenzweig <srosenzweig09@gmail.com>
2021-08-07 14:02:09 -0400

    Developing script to produce rate plots.

--- displacedmuons/scripts/rate_plots.py
+++ displacedmuons/scripts/rate_plots.py
@@ -1,19 +1,19 @@
-from configparser import ConfigParser
-import awkward as ak
-from icecream import ic
+# from configparser import ConfigParser
+# import awkward as ak
+# from icecream import ic
 import numpy as np
 np.seterr(all='ignore')
-import matplotlib as mpl
-from matplotlib.lines import Line2D
-import matplotlib.pyplot as plt
+# import matplotlib as mpl
+# from matplotlib.lines import Line2D
+# import matplotlib.pyplot as plt
 from myuproot import open_up
-import sys
+# import sys
 
-N_bx = 2736 # IP5, CMS
+N_bx = 2736 # IP5, CMS (Run 3)
 # https://indico.cern.ch/event/751857/contributions/3259413/attachments/1781638/3257666/ExperimentsInRun3_proceedings.pdf
-f_LHC = 
+f_LHC = 11.246; # khZ
 
-filename = '/eos/cms/store/user/eyigitba/emtf/L1Ntuples/Run3/L1TMenuStudies/Run3Rates/NuGun_11.05.21/nuGun_11May21.root'
+filename = '/eos/cms/store/user/eyigitba/emtf/L1Ntuples/Run3/crabOut/Nu_E10-pythia8-gun/NuGun_11_3_0_pre5_NNv6_5M/210421_154733/NuGun_11_3_0_pre5_NNv6_5M.root'
 
-tree, ak_table, np_table = open_up(filename, 'tree')
+tree, ak_table, np_table = open_up(filename, 'EMTFNtuple/tree')
 
